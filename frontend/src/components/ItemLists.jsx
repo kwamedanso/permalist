4bf51aa7207abd3dafffb8be2284c48e862cacfe
kwamedanso <dansokwame96@gmail.com>
2025-02-25 08:19:10 +0000

    Component displays the all todo items.

---
+++ frontend/src/components/ItemLists.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from 'react'
+import axios from 'axios'
+import Item from './Item'
+import { ItemContext } from './ItemListContext'
+
+export default function ItemLists() {
+    const { state, dispatch } = useContext(ItemContext)
+
+    useEffect(() => {
+
+        async function getData() {
+            const response = await axios.get("/api");
+            const json = await response.data;
+            dispatch({ type: "fetch-data", payload: { items: json } })
+        }
+
+        getData()
+    }, [])
+
+
+    return (
+        <>
+            {state?.map(item => <Item key={item.id} id={item.id} title={item.title} />)}
+        </>
+    )
+}
