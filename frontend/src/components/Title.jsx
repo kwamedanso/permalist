f6946c366a79c63ea99208d799d481596e5c7dc4
kwamedanso <dansokwame96@gmail.com>
2025-02-25 08:20:31 +0000

    Title component for the todo list.

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="box" id="heading">
+            <h1>
+                Today
+            </h1>
+        </div>
+    )
+}
