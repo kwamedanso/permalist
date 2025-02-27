b26ade710bc5bd7a13efcc9ed443b1845136a7e4
kwamedanso <dansokwame96@gmail.com>
2025-02-27 12:57:19 +0000

    Add the edit route and improve on the error handling of the other routes when querying the database

--- backend/server.js
+++ backend/server.js
@@ -18,38 +18,62 @@ const db = new pg.Pool({
 db.connect()
 
 
-app.get("/api", async (req, res) => {
+app.get("/api/all", async (req, res) => {
     try {
-        const response = await db.query("SELECT * FROM items")
+        const response = await db.query("SELECT * FROM items ORDER BY id ASC");
         const items = response.rows
-        res.send(items)
+        res.json(items)
     } catch (error) {
         console.log(error)
     }
 })
 
-app.post("/api/post", async (req, res) => {
+app.post("/api/post/new", async (req, res) => {
     const { title } = req.body;
+
     try {
         const response = await db.query("INSERT INTO items(title) VALUES($1) RETURNING *", [title])
-        res.send(response.rows[0])
+        if (response.rowCount !== 1) {
+            res.status(404).json({ error: "Unable to add new item" })
+        }
+        res.status(200).json({ message: "Item added successfully", item: response.rows[0] })
 
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/api/post/edit", async (req, res) => {
+    const { id, title } = req.body;
+
+    try {
+        const response = await db.query(`UPDATE items SET title='${title}' WHERE id=${id} RETURNING *`)
+        console.log(response.rows)
+        if (response.rowCount !== 1) {
+            return res.status(404).json({ error: "Enable to update item" })
+        }
+        return res.status(200).json({ message: "Item updated successfully", item: response.rows[0] })
     } catch (error) {
         console.log(error)
     }
 })
 
 app.delete("/api/delete/:id", async (req, res) => {
-    const { id } = req.params;
+    let { id } = req.params;
 
     try {
-        const response = await db.query("DELETE FROM items WHERE id = $1", [id])
-        res.status(200).send("Item Deleted")
+        const response = await db.query("DELETE FROM items WHERE id = $1 RETURNING *", [Number(id)])
+        if (response.rowCount !== 1) {
+            return res.status(404).json({ error: "Item not found" })
+        }
+
+        return res.status(200).json({ message: "Item deleted successfully", deletedItem: response.rows[0] })
 
     } catch (error) {
         console.log(error)
     }
-})
+}
+)
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`)
